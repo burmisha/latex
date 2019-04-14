55a236138f9f0c197d1c75f3cf032e99dc7a6584
Mikhail Burmistrov <burmisha@gmail.com>
2019-04-14 11:53:41 +0200

    Move other tasks to python

--- problems/__init__.py
+++ problems/__init__.py
@@ -1,3 +1,5 @@
 import gendenshteyn7
 import gendenshteyn8
+import gendenshteyn10
 import getaclass
+import savchenko
---
+++ problems/savchenko.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import task
+
+
+class Savchenko(task.TasksGenerator):
+    def GetBookName(self):
+        return 'savchenko'
+
+    def __call__(self):
+        tasks = {
+            '6-4-4': ur'''
+                Площадь обкладок плоского конденсатора $S$, расстояние между пластинами $d$.
+                \begin{itemize}
+                    \item Определите его ёмкость.
+                    \item Как изменится ёмкость конденсатора, если между его обкладками поместить металлическую пластину толщины $\tfrac d3$ и площади $S$?
+                    \item  Как изменится ёмкость конденсатора, если между его обкладками поместить металлическую пластину той же толщины $\tfrac d3$, но площади $S' < S$?
+                    \item  Изменится ли ёмкость конденсатора, если эта пластина коснётся одной из обкладок?
+                \end{itemize}
+            ''',
+        }
+        for number, text in tasks.iteritems():
+            yield task.Task(text, number=number)
