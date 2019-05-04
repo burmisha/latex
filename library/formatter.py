6432a2657cf53f1d4e6a0e5a4445aaf22f0c2f88
Mikhail Burmistrov <burmisha@gmail.com>
2019-05-04 12:03:03 +0300

    More asserts

--- library/formatter.py
+++ library/formatter.py
@@ -31,9 +31,11 @@ class Date(object):
             '10': u'октября',
             '11': u'ноября',
             '12': u'декабря',
-        }
+        }[month]
         day = int(day)
-        return u'{} {} {}'.format(int(day), textMonth[month], year)
+        assert 1 <= day <= 31, 'Error on %r' % self.__DateStr
+        assert 2018 <= int(year) <= 2019, 'Error on %r' % self.__DateStr
+        return u'{} {} {}'.format(int(day), textMonth, year)
 
 
 class TextFormatter(object):
@@ -41,19 +43,19 @@ class TextFormatter(object):
         assert text.strip()
         self.Text = text.strip('\n')
 
-    def FirstSpacesCount(self, line):
+    def __FirstSpacesCount(self, line):
         return len(line) - len(line.lstrip(' '))
 
     def Format(self, addIndent=0):
         lines = self.Text.split('\n')
-        minSpaces = min(self.FirstSpacesCount(line) for line in lines if line.strip())
+        minSpaces = min(self.__FirstSpacesCount(line) for line in lines if line.strip())
         lastSpaces = None
         for line in lines:
             line = line[minSpaces:].rstrip(' ')
             if line.strip():
                 log.debug('Line: %r', line)
                 line = line.replace('. ', '.\n')
-                spaces = self.FirstSpacesCount(line)
+                spaces = self.__FirstSpacesCount(line)
                 log.debug('Line: |%s|, spaces: %d', line, spaces)
                 for newLine in line.split('\n'):
                     yield ' ' * (addIndent + spaces) + newLine.strip(' ')
