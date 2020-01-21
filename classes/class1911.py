2293aa8e1f5748208ef318889f68b0e75b30d1c5
Mikhail Burmistrov <burmisha@gmail.com>
2020-01-22 00:20:08 +0300

    More cheshev for 11

--- classes/class1911.py
+++ classes/class1911.py
@@ -8,8 +8,8 @@ class Class1911(paper.PaperGenerator):
     def __call__(self):
         papers = {
             '2019-01-22': [
-                ('cheshev', ['5-01']),
+                ('cheshev', ['5-01', '5-02', '5-03', '5-04']),
             ],
         }
         for date, tasks in papers.iteritems():
-            yield paper.Paper(date, tasks, classLetter='11', style=ur'\twocolumns{40pt}{\twovariants{40pt}{\variant}}{\twovariants{20pt}{\variant}}')
+            yield paper.Paper(date, tasks, classLetter='11', style=ur'\twovariants{60pt}{\variant}')
