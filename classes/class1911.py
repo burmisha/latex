e012c88413c77df01b6702b9d4b0c0f3cce96bb0
Mikhail Burmistrov <burmisha@gmail.com>
2020-01-22 00:31:08 +0300

    Fix date

--- classes/class1911.py
+++ classes/class1911.py
@@ -7,7 +7,7 @@ import paper
 class Class1911(paper.PaperGenerator):
     def __call__(self):
         papers = {
-            '2019-01-22': [
+            '2020-01-22': [
                 ('cheshev', ['5-01', '5-02', '5-03', '5-04']),
             ],
         }
