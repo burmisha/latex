85da7debfcbcfa006e59c5b60d70234c9e169a21
Mikhail Burmistrov <burmisha@gmail.com>
2020-05-04 14:09:18 +0300

    Simplify koleb

--- generators/koleb.py
+++ generators/koleb.py
@@ -1,67 +1,58 @@
 # -*- coding: utf-8 -*-
 
-import itertools
-import logging
-
 import problems
 import variant
 from value import UnitValue
 
+import logging
 log = logging.getLogger(__name__)
 
 
 class Nu01(variant.VariantTask):
-    def __call__(self, free=None, const=None):
+    def __call__(self, **kws):
         text = u'''
             Запишите определения:
-            \\begin{itemize}
+            \\begin{{itemize}}
                 \\item гармонические колебания,
-        '''
-        if free:
-            text += u'''\item   свободные колебания,'''
-        else:
-            text += u'''\item   вынужденные колебания,'''
-        if const:
-            text += u'''\item   незатухающие колебания,'''
-        else:
-            text += u'''\item   затухающие колебания.'''
-        text += u'''
-            \\end{itemize}
-        '''
+                \\item {free},
+                \\item {const}.
+            \\end{{itemize}}
+        '''.format(**kws)
         return problems.task.Task(text)
 
-    def All(self):
-        for free, const in itertools.product(
-            [False, True],
-            [False, True],
-        ):
-            yield self.__call__(free=free, const=const)
+    def GetArgs(self):
+        return {
+            'free': [u'свободные колебания', u'вынужденные колебания'],
+            'const': [u'незатухающие колебания', u'затухающие колебания'],
+        }
 
 
 class Nu02(variant.VariantTask):
-    def __call__(self, T=None):
+    def __call__(self, **kws):
+        T = kws['T']
         text = u'''
             Определите частоту колебаний, если их период составляет {T:Task:e}.
-        '''.format(T=T)
+        '''.format(**kws)
         nu = UnitValue(u'''\\nu = %d Гц''' % (1000 / T.Value))
         answer = u'''
             $\\nu = \\frac 1T = \\frac 1{T:Value:s} = {nu:Value}$
         '''.format(T=T, nu=nu)
         return problems.task.Task(text, answer=answer)
 
-    def All(self):
-        for T in itertools.product(
-            [2, 4, 5, 10, 20, 40, 50],
-        ):
-            yield self.__call__(T=UnitValue(u'T = %d мс' % T))
+    def GetArgs(self):
+        return {
+            'T': [UnitValue(u'T = %d мс' % T) for T in [2, 4, 5, 10, 20, 40, 50]],
+        }
 
 
 class Nu03(variant.VariantTask):
-    def __call__(self, nu=None, t=None):
+    def __call__(self, **kws):
         text = u'''
             Определите период колебаний, если их частота составляет {nu:Task:e}.
             Сколько колебаний произойдёт за {t:Task:e}?
-        '''.format(nu=nu, t=t)
+        '''.format(**kws)
+        nu = kws['nu']
+        t = kws['t']
         T = UnitValue(u'''T = %.3f мc''' % (1. / nu.Value))
         N = UnitValue(u'''N = %d колебаний''' % (nu.Value * 1000 * t.Value * 60))
         answer = u'''
@@ -72,23 +63,22 @@ class Nu03(variant.VariantTask):
         '''.format(T=T, nu=nu, N=N, t=t)
         return problems.task.Task(text, answer=answer)
 
-    def All(self):
-        for nu, t in itertools.product(
-            [2, 4, 5, 10, 20, 40, 50],
-            [1, 2, 3, 5, 10],
-        ):
-            yield self.__call__(
-                nu=UnitValue(u'\\nu = %d кГц' % nu),
-                t=UnitValue(u't = %d мин' % t),
-            )
+    def GetArgs(self):
+        return {
+            'nu': [UnitValue(u'\\nu = %d кГц' % nu) for nu in [2, 4, 5, 10, 20, 40, 50]],
+            't': [UnitValue(u't = %d мин' % t) for t in [1, 2, 3, 5, 10]],
+        }
 
 
 class Nu04(variant.VariantTask):
-    def __call__(self, A=None, nu=None, t=None):
+    def __call__(self, **kws):
+        A = kws['A']
+        nu = kws['nu']
+        t = kws['t']
         text = u'''
             Амплитуда колебаний точки составляет {A:Task:e}, а частота~--- {nu:Task:e}.
             Определите, какой путь преодолеет эта точка за {t:Task:e}.
-        '''.format(A=A, nu=nu, t=t)
+        '''.format(**kws)
         s = UnitValue(u'%.1f м' % (4. * A.Value / 100 * t.Value * nu.Value))
         answer = u'''$
             s
@@ -98,33 +88,24 @@ class Nu04(variant.VariantTask):
         $'''.format(A=A, nu=nu, t=t, s=s)
         return problems.task.Task(text, answer=answer)
 
-    def All(self):
-        for A, nu, t in itertools.product(
-            [2, 3, 5, 10, 15],
-            [2, 5, 6, 10, 20],
-            [10, 40, 80],
-        ):
-            yield self.__call__(
-                A=UnitValue(u'A = %d см' % A),
-                nu=UnitValue(u'\\nu = %d Гц' % nu),
-                t=UnitValue(u't = %d с' % t),
-            )
+    def GetArgs(self):
+        return {
+            'A': [UnitValue(u'A = %d см' % A) for A in [2, 3, 5, 10, 15]],
+            'nu': [UnitValue(u'\\nu = %d Гц' % nu) for nu in [2, 5, 6, 10, 20]],
+            't': [UnitValue(u't = %d с' % t) for t in [10, 40, 80]],
+        }
 
 
 class Nu05(variant.VariantTask):
-    def __call__(self, A=None, T=None):
+    def __call__(self, **kws):
         text = u'''
             Изобразите график гармонических колебаний, амплитуда которых составляла бы
             {A:Task:e}, а период {T:Task:e}.
-        '''.format(A=A, T=T)
+        '''.format(**kws)
         return problems.task.Task(text)
 
-    def All(self):
-        for A, T in itertools.product(
-            [1, 2, 3, 5, 6, 15, 30, 40, 75],
-            [2, 4, 6, 8, 10],
-        ):
-            yield self.__call__(
-                A=UnitValue(u'A = %d см' % A),
-                T=UnitValue(u'T = %d с' % T),
-            )
+    def GetArgs(self):
+        return {
+            'A': [UnitValue(u'A = %d см' % A) for A in [1, 2, 3, 5, 6, 15, 30, 40, 75]],
+            'T': [UnitValue(u'T = %d с' % T) for T in [2, 4, 6, 8, 10]],
+        }
