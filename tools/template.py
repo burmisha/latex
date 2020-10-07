0d2833dd9033d4aa90ec02c91d2205fb1f558443
Mikhail Burmistrov <burmisha@gmail.com>
2020-10-08 00:56:35 +0300

    Convert docx2pdf with Word and AppleScript

--- tools/template.py
+++ tools/template.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import itertools
+import library
 import os
 import shutil
-import library
-import itertools
+import subprocess
 
 import logging
 log = logging.getLogger(__name__)
@@ -40,6 +41,78 @@ class FileCopier(object):
             log.debug(u'File %s already exists', destination_path)
 
 
+class DocxToPdf(object):
+    def __init__(self):
+        self.__GroupContainerDir = os.path.join(os.environ['HOME'], 'Library', 'Group Containers', 'UBF8T346G9.Office')
+        assert os.path.exists(self.__GroupContainerDir)
+        assert os.path.isdir(self.__GroupContainerDir)
+
+    def ConvertFile(self, source_file, destination_file):
+        log.info('Converting \'%s\' to \'%s\'', source_file, destination_file)
+        assert os.path.exists(source_file)
+        assert os.path.isfile(source_file)
+        assert source_file.endswith('.docx')
+        assert destination_file.endswith('.pdf')
+        assert not os.path.exists(destination_file)
+
+        tmp_docx_file = os.path.join(self.__GroupContainerDir, '_convert_tmp.docx')
+        tmp_pdf_file = os.path.join(self.__GroupContainerDir, '_convert_tmp.pdf')
+        for file in [tmp_docx_file, tmp_pdf_file]:
+            if os.path.exists(file):
+                assert os.path.isfile(file), u'Expected file: \'%s\'' % file
+                os.remove(file)
+
+        shutil.copy(source_file, tmp_docx_file)
+        apple_script = '''
+            tell application "Microsoft Word"
+                activate
+                set tmp_docx_file to "{tmp_docx_file}"
+                set tmp_pdf_file to "{tmp_pdf_file}"
+                open tmp_docx_file
+                save as active document file name tmp_pdf_file file format format PDF
+                close active window saving no
+            end tell
+        '''.format(
+            tmp_docx_file=tmp_docx_file,
+            tmp_pdf_file=tmp_pdf_file,
+        )
+
+        # https://stackoverflow.com/questions/2940916/how-do-i-embed-an-applescript-in-a-python-script
+        # https://discussions.apple.com/thread/7571530
+        # https://superuser.com/questions/338165/convert-batch-of-word-files-to-pdfs-in-mac-os-x
+        # https://stackoverflow.com/questions/51844514/macos-automator-applescript-solution-for-exporting-docx-to-pdf
+        # https://stackoverflow.com/questions/16534292/basic-powershell-batch-convert-word-docx-to-pdf
+        # https://apple.stackexchange.com/questions/59532/create-automator-service-with-a-python-script
+        # https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/AppleScriptX/Concepts/work_with_as.html#//apple_ref/doc/uid/TP40001568-BABEBGCF
+
+        p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate(apple_script)
+        assert p.returncode == 0, u'returncode expected to by 0, got %s (%r)' % p.returncode
+        assert stdout == '', u'stdout expected to by empty, got %s (%r)' % stdout
+        assert stderr == '', u'stderr expected to by empty, got %s (%r)' % stderr
+
+        assert os.path.exists(tmp_pdf_file)
+        assert os.path.isfile(tmp_pdf_file)
+
+        shutil.move(tmp_pdf_file, destination_file)
+        log.info('Converted \'%s\' to \'%s\'', source_file, destination_file)
+        os.remove(tmp_docx_file)
+
+    def ConvertDir(self, directory):
+        assert os.path.exists(directory)
+        assert os.path.isdir(directory)
+        docx_suffix = '.docx'
+        new_converted = 0
+        already_converted_count = 0
+        for docx_file in sorted(library.files.walkFiles(directory, extensions=[docx_suffix])):
+            pdf_file = docx_file[:-len(docx_suffix)] + '.pdf'
+            if not os.path.exists(pdf_file):
+                self.ConvertFile(docx_file, pdf_file)
+                new_converted += 1
+            else:
+                already_converted_count += 1
+        log.info(u'Converted %d files (and found %d existing) in %s', new_converted, already_converted_count, directory)
+
 
 def runTemplate(args):
     fileCopier = FileCopier(library.files.udrPath('template-2-columns.docx'))
@@ -50,9 +123,9 @@ def runTemplate(args):
         # u'1.3 - Статика',
         # u'1.4 - Законы сохранения',
         # u'1.5 - Колебания и волны',
-        u'2.1 - Молекулярная физика',
-        u'2.2 - Термодинамика',
-        u'3.1 - Электрическое поле',
+        # u'2.1 - Молекулярная физика',
+        # u'2.2 - Термодинамика',
+        # u'3.1 - Электрическое поле',
         # u'3.2 - Законы постоянного тока',
         # u'3.3 - Магнитное поле',
         # u'3.4 - Электромагнитная индукция',
@@ -75,6 +148,11 @@ def runTemplate(args):
         filename = u'Вишнякова - %s - %s - %s.docx' % (chapter, course, file_type)
         fileCopier.CreateFile(filename)
 
+    if args.docx_2_pdf:
+        docxToPdf = DocxToPdf()
+        docxToPdf.ConvertDir(library.files.udrPath(u'11 класс', u'2020 весна'))
+
 
 def populate_parser(parser):
+    parser.add_argument('--docx-2-pdf', help='Run converter too', action='store_true')
     parser.set_defaults(func=runTemplate)
