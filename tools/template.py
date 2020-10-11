4e4188e992fa834218072732d57dd3fb215538f2
Mikhail Burmistrov <burmisha@gmail.com>
2020-10-11 11:18:52 +0300

    Delete deprecated code

--- tools/template.py
+++ tools/template.py
@@ -120,25 +120,6 @@ def runTemplate(args):
         filename = u'Вишнякова - %s - %s - %s.docx' % (chapter, course, file_type)
         fileCopier.CreateFile(filename)
 
-    if args.docx_2_pdf:
-        docxToPdf = DocxToPdf()
-        docxToPdf.ConvertDir(
-            library.files.udrPath(u'11 класс', u'2020 весна'),
-            recursive=False,
-            regexp=u'.*Вишнякова - [0-9].*',
-        )
-        docxToPdf.ConvertDir(
-            library.files.udrPath(u'10 класс'),
-            destination_directory=u'2020-21 10AБ Физика',
-            recursive=False,
-            regexp=u'.*Неделя.*',
-        )
-        docxToPdf.ConvertDir(
-            library.files.udrPath(u'10 класс'),
-            recursive=False,
-            regexp=u'.*Рабочая тетрадь.*',
-        )
-
     if args.run_extractor:
         pdfExtractor = PdfExtractor(library.files.udrPath(u'10 класс', u'10-1 - Кинематика - Рабочая тетрадь.pdf'))
         pdfExtractor.Extract('5, 1-4', library.files.udrPath(u'10 класс', u'tmp.pdf'))
