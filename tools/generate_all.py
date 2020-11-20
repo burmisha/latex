8d19646635e8fa35b55364ffdf7aca08499c62e4
Mikhail Burmistrov <burmisha@gmail.com>
2020-11-20 23:12:51 +0300

    Start forming task sets

--- tools/generate_all.py
+++ tools/generate_all.py
@@ -10,6 +10,13 @@ import tools
 import logging
 log = logging.getLogger(__name__)
 
+
+class TaskSet:
+    def __init__(self, date, variant_tasks):
+        self._date = date
+        self._variant_tasks = variant_tasks
+
+
 def run(args):
     fileWriter = library.files.FileWriter(args.filter)
 
@@ -56,117 +63,117 @@ def run(args):
         seed = 2704
 
         classRandomTasks = {
-            '2018-10': {
-                '2019-04-16': [
+            '2018-10': [
+                TaskSet('2019-04-16', [
                     generators.electricity.ForceTask(),
                     generators.electricity.ExchangeTask(),
                     generators.electricity.FieldTaskGenerator(),
                     generators.electricity.SumTask(),
-                ],
-                '2019-04-30': [
+                ]),
+                TaskSet('2019-04-30', [
                     generators.electricity.Potential728(),
                     generators.electricity.Potential735(),
                     generators.electricity.Potential737(),
                     generators.electricity.Potential2335(),
                     generators.electricity.Potential1621(),
-                ],
-                '2019-05-06': [
+                ]),
+                TaskSet('2019-05-06', [
                     generators.electricity.Rymkevich748(),
                     generators.electricity.Rymkevich750(),
                     generators.electricity.Rymkevich751(),
                     generators.electricity.Rymkevich762(),
                     generators.electricity.Cond1(),
-                ],
-                '2019-05-14': [
+                ]),
+                TaskSet('2019-05-14', [
                     generators.electricity.Rezistor1_v1(),
                     generators.electricity.Rezistor2(),
                     generators.electricity.Rezistor3(),
                     generators.electricity.Rezistor4(),
-                ],
-            },
-            '2018-11': {
-                '2019-04-19': [
+                ]),
+            ],
+            '2018-11': [
+                TaskSet('2019-04-19', [
                     generators.quantum.Fotons(),
                     generators.quantum.KernelCount(),
                     generators.quantum.RadioFall(),
                     generators.quantum.RadioFall2(),
-                ],
-                '2019-04-30': [
+                ]),
+                TaskSet('2019-04-30', [
                     generators.quantum.Quantum1119(),
                     generators.quantum.Quantum1120(),
-                ],
-            },
-            '2019-8': {
-                '2019-11-27': [
+                ]),
+            ],
+            '2019-8': [
+                TaskSet('2019-11-27', [
                     generators.termo.Ch_8_6(),
                     generators.termo.Ch_8_7(),
                     generators.termo.Ch_8_10(),
                     generators.termo.Ch_8_13(),
                     generators.termo.Ch_8_35(),
-                ],
-            },
-            '2019-9-А': {
-                '2019-11-25': [
+                ]),
+            ],
+            '2019-9-А': [
+                TaskSet('2019-11-25', [
                     generators.mechanics.Ch_3_1(),
                     generators.mechanics.Ch_3_2(),
                     generators.mechanics.Ch_3_3(),
                     generators.mechanics.Ch_3_24(),
                     generators.mechanics.Ch_3_26(),
-                ],
-                '2019-12-17': [
+                ]),
+                TaskSet('2019-12-17', [
                     generators.koleb.Nu01(),
                     generators.koleb.Nu02(),
                     generators.koleb.Nu03(),
                     generators.koleb.Nu04(),
                     generators.koleb.Nu05(),
-                ],
-                '2019-12-24': [
+                ]),
+                TaskSet('2019-12-24', [
                     generators.waves.Waves00(),
                     generators.waves.Waves03(),
                     generators.waves.Waves04(),
                     generators.waves.Waves05(),
-                ],
-                '2020-04-22': [
+                ]),
+                TaskSet('2020-04-22', [
                     generators.vishnyakova.BK_53_01(),
                     generators.vishnyakova.BK_53_02(),
                     generators.vishnyakova.BK_53_03(),
                     generators.vishnyakova.BK_53_12(),
-                ],
-            },
-            '2019-9-Л': {
-                '2020-01-20': [
+                ]),
+            ],
+            '2019-9-Л': [
+                TaskSet('2020-01-20', [
                     generators.koleb.Nu01(),
                     generators.koleb.Nu02(),
                     generators.koleb.Nu03(),
                     generators.koleb.Nu04(),
                     generators.koleb.Nu05(),
-                ],
-                '2020-04-28': [
+                ]),
+                TaskSet('2020-04-28', [
                     generators.optics.Gendenshteyn_11_11_18(),
                     generators.vishnyakova.BK_52_01(),
                     generators.vishnyakova.BK_52_02(),
                     generators.vishnyakova.BK_52_07(),
-                ],
-            },
-            '2019-11-Т': {
-                '2019-09-11': [
+                ]),
+            ],
+            '2019-11-Т': [
+                TaskSet('2019-09-11', [
                     generators.magnet.ConstMagnet0(),
                     generators.magnet.ConstMagnet1(),
                     generators.magnet.ConstMagnet2(),
                     generators.magnet.ConstMagnet3(),
-                ],
-                '2019-11-13': [
+                ]),
+                TaskSet('2019-11-13', [
                     generators.waves.Waves01(),
                     generators.waves.Ch1238(),
                     generators.waves.Ch1240(),
                     generators.waves.Waves02(),
-                ],
-                '2020-03-04': [
+                ]),
+                TaskSet('2020-03-04', [
                     generators.optics.Gendenshteyn_11_11_18(),
                     generators.optics.Vishnyakova_example_11(),
                     generators.optics.Belolipetsky_5_196(),
-                ],
-                '2020-04-29': [
+                ]),
+                TaskSet('2020-04-29', [
                     # sto
                     generators.sto.Equations(),
                     generators.vishnyakova.BK_4_01(),
@@ -182,35 +189,36 @@ def run(args):
                     generators.vishnyakova.BK_53_02(),
                     generators.vishnyakova.BK_53_03(),
                     generators.vishnyakova.BK_53_12(),
-
-                ],
-            },
-            '2019-11S': {
-                '2019-09-30': [
+                ]),
+            ],
+            '2019-11S': [
+                TaskSet('2019-09-30', [
                     generators.magnet.Chernoutsan11_01(),
                     generators.magnet.Chernoutsan11_02(),
                     generators.magnet.Chernoutsan11_5(),
-                ],
-            },
-            '2020-10': {
-                '2020-09-10': [
+                ]),
+            ],
+            '2020-10': [
+                TaskSet('2020-09-10', [
                     generators.mechanics.Theory_1(),
                     generators.mechanics.Vectors_SumAndDiff(),
                     generators.mechanics.Chernoutsan_1_2(),
                     generators.mechanics.Vectors_SpeedSum(),
-                ],
-            },
-            '2020-9': {
-                '2020-09-10': [
+                ]),
+            ],
+            '2020-9': [
+                TaskSet('2020-09-10', [
                     generators.mechanics.Theory_1_simple(),
                     generators.mechanics.Chernoutsan_1_2(),
                     generators.mechanics.Chernoutsan_1_2_1(),
-                ],
-            },
+                ]),
+            ],
         }
-        for className, dateTasks in classRandomTasks.items():
+        for className, task_sets in classRandomTasks.items():
             pupils = library.pupils.getPupils(className, addMyself=True, onlyMe=args.me)
-            for date, variantTasks in dateTasks.items():
+            for task_set in task_sets:
+                date = task_set._date
+                variantTasks = task_set._variant_tasks
                 multiplePaper = generators.variant.MultiplePaper(date, pupils=pupils)
                 filename = multiplePaper.GetFilename()
                 if fileWriter.NotMatches(filename):
