a041fd527d1b7c0c9215de63e551b0b9681cef0b
Mikhail Burmistrov <burmisha@gmail.com>
2020-12-18 02:01:31 +0300

    Search best dir, wait for the new file

--- tools/yaform.py
+++ tools/yaform.py
@@ -3,6 +3,7 @@ import library.location
 import library.logging
 import library.pupils
 import library.secrets
+from library.logging import colorize_json, cm, color
 
 import collections
 import json
@@ -103,13 +104,10 @@ class AnswersJoiner:
                 for pupil_name, links in answers.items():
                     for index, link in enumerate(links, 1):
                         link_basename = link.split('_', 1)[1]
-                        file_name = os.path.join(
-                            pupils_dir,
-                            work_name,
-                            f'{pupil_name} - {index:02d} - {link_basename}',
-                        )
-                        description.add_pupil(pupil_name)
-                        yield link, file_name
+                        dir_name = os.path.join(pupils_dir, work_name)
+                        file_name = f'{pupil_name} - {index:02d} - {link_basename}'
+                        yield dir_name, pupil_name, link, file_name
+                    description.add_pupil(pupil_name)
                 description.save()
 
     def _already_downloaded(self, file_name):
@@ -121,26 +119,39 @@ class AnswersJoiner:
         existing_files = list(library.files.walkFiles(dir_name, regexp=base_name))
         if existing_files:
             assert len(existing_files) == 1
-            log.info(f'{file_name} was downloaded to {existing_files[0]}')
+            log.debug(f'{file_name} was downloaded to {existing_files[0]}')
             return True
         else:
             return False
 
+    def _get_best_dir(self, dir_name, pupil_name):
+        existing_files = list(library.files.walkFiles(dir_name, regexp=f'{pupil_name}.*'))
+        if existing_files:
+            dir_names = list(set(os.path.dirname(existing_file) for existing_file in existing_files))
+            if len(dir_names) == 1:
+                return dir_names[0]
+
+        return dir_name
+
     def Download(self):
         log.info('Downloading all files')
         count, existing = 0, 0
-        for link, file_name in self._get_download_cfg():
-            if not self._already_downloaded(file_name):
-                os.makedirs(os.path.dirname(file_name), exist_ok=True)
-                self._download_to_file(link, file_name)
+        for dir_name, pupil_name, link, file_name in self._get_download_cfg():
+            best_dir = self._get_best_dir(dir_name, pupil_name)
+            full_name = os.path.join(best_dir, file_name)
+            if not self._already_downloaded(full_name):
+                if not os.path.isdir(best_dir):
+                    log.info(f'Creating {best_dir}')
+                    os.makedirs(os.path.dirname(best_dir), exist_ok=True)
+                self._download_to_file(link, full_name)
                 count += 1
             else:
-                log.debug(f'File {file_name} already exists')
+                log.debug(f'File {file_name} for {pupil_name} already exists')
                 existing += 1
 
         log.info(f'Downloaded {count} files and got {existing} existing ones')
 
-    def sync_with_yadisk(self, form_id=None, wait_time=10):
+    def sync_with_yadisk(self, form_id=None):
         data = {
             'method': 'POST',
             'path[name]': 'profile-survey-answers:export',
@@ -159,14 +170,12 @@ class AnswersJoiner:
             'query[survey]': form_id,
         }
         response = requests.post('https://forms.yandex.ru/admin/_api', headers=self._csrf_headers, data=data)
-
-        log.info([response.url, data])
         if response.ok:
             task_id = response.json()['task_id']
-            log.info(f'Got task id {task_id}. Waiting for {wait_time} seconds')
-            time.sleep(wait_time)
+            log.info(f'Got task id {task_id}')
         else:
-            raise RuntimeError(f'failed to start sync task: {response}, {response.content}')
+            log.error(f'Error at {response.url} and {colorize_json(data)}: {response.content}')
+            raise RuntimeError(f'Failed to start sync task: {response}')
 
 
 class YFAnswer:
@@ -212,20 +221,38 @@ class YFAnswer:
         return f'[{self._create_time}][{len(self.get_photos())} files] {self._work_name}: {self._original_name}'
 
 
+def get_latest_file(dir_name, regexp):
+    candidates = library.files.walkFiles(dir_name, regexp=regexp)
+    candidates = sorted(candidates, key=lambda f: os.path.getmtime(f))
+    if candidates:
+        candidate = candidates[-1]
+        log.info(f'File with largest mtime: {cm(candidate, color=color.Cyan)} ')
+        return candidate
+    else:
+        return None
+
+
 def run(args):
+    sleep_time = args.sleep
     form_id = '5fd491a3dfc5aebea76233ef'
     log.info(f'Forse JSON update at https://forms.yandex.ru/admin/{form_id}/answers')
     log.info('Delete old versions at https://disk.yandex.ru/client/disk/Yandex.Forms')
 
+    answer_location = library.location.ya_disk('Yandex.Forms')
+    regexp = r'.*202021 Физика 554.*\.json'
+    old_latest_file = get_latest_file(answer_location, regexp)
+    new_latest_file = None
+
     answers_joiner = AnswersJoiner()
     if args.sync:
         answers_joiner.sync_with_yadisk(form_id)
+        new_latest_file = get_latest_file(answer_location, regexp)
+        while (new_latest_file is None) or (new_latest_file == old_latest_file):
+            log.info(f'Waiting for {sleep_time} seconds for the new file to sync')
+            time.sleep(sleep_time)
+            new_latest_file = get_latest_file(answer_location, regexp)
 
-    answer_location = library.location.ya_disk('Yandex.Forms')
-    candidates = library.files.walkFiles(answer_location, regexp=r'.*202021 Физика 554.*\.json')
-    yandex_form_file = sorted(candidates)[-1]
-    log.info(f'Using {yandex_form_file} as having largest filename')
-
+    yandex_form_file = new_latest_file or old_latest_file
     with open(yandex_form_file) as f:
         yandex_form_raw = json.load(f)
 
@@ -238,4 +265,5 @@ def run(args):
 
 def populate_parser(parser):
     parser.add_argument('-s', '--sync', help='Sync form data from yandex server to Yandex.Disk', action='store_true')
+    parser.add_argument('--sleep', help='Sleep time for syncing', default=5, type=int)
     parser.set_defaults(func=run)
