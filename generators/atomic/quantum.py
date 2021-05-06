c1f0df0a584495b340348ffcafc8a7ba64147e8f
Mikhail Burmistrov <burmisha@gmail.com>
2021-05-06 20:58:24 +0300

    Add answers

--- generators/atomic/quantum.py
+++ generators/atomic/quantum.py
@@ -11,7 +11,10 @@ from generators.helpers import UnitValue, Consts
     Длина волны излучения {length:Value:e}. {h:Task:e}.
 ''')
 @variant.answer_short('''
-    N = \\frac{ Pt\\lambda }{ hc }
+    N 
+        = \\frac{ E_{ \\text { общая } } }{ E_{ \\text { одного фотона } } }
+        = \\frac{ Pt }{ h\\nu } = \\frac{ Pt }{ h \\frac c { \\lambda } }
+        = \\frac{ Pt\\lambda }{ hc }
        = \\frac{
             {power:Value} * {minutes} * 60 \\units{ с } * {length:Value}
          }{
@@ -59,12 +62,32 @@ class Fotons(variant.VariantTask):
         \\item {q4:V:e}.
     \\end{{enumerate}}
 ''')
+@variant.answer_tex('''
+    \\begin{{enumerate}}
+        \\item ${q1:V} \\to$ {a1}, $\\nu_1 = \\frac c{ \\lambda_1 } \\approx {nu1:V}$,
+        \\item ${q2:V} \\to$ {a2}, $\\nu_2 = \\frac c{ \\lambda_2 } \\approx {nu2:V}$,
+        \\item ${q3:V} \\to$ {a3}, $\\nu_3 = \\frac c{ \\lambda_3 } \\approx {nu3:V}$,
+        \\item ${q4:V} \\to$ {a4}, $\\nu_4 = \\frac c{ \\lambda_4 } \\approx {nu4:V}$.
+    \\end{{enumerate}}
+
+    $\\nu = \\frac 1 T = \\frac c{ \\lambda } = \\frac {Consts.c_4:Value:s}{ l * {mkm:V} } \\approx \\frac{nu_0:V:s}l$, 
+    где $l$~--- численное значение длины волны в мкм.
+''')
 @variant.arg(q1__a1=[('450 нм', 'синий'), ('580 нм', 'жёлтый '), ('660 нм', 'красный')])
-@variant.arg(q2__a2=[('470 нм', 'синий'), ('390 нм', 'фиолетовый'), ('595 нм', 'оранжевый ')])
+@variant.arg(q2__a2=[('470 нм', 'синий'), ('390 нм', 'фиолетовый'), ('595 нм', 'оранжевый')])
 @variant.arg(q3__a3=[('530 нм', 'зелёный'), ('720 нм', 'красный'), ('610 нм', 'оранжевый')])
 @variant.arg(q4__a4=[('580 нм', 'зелёный'), ('490 нм', 'голубой'), ('420 нм', 'фиолетовый')])
 class ColorNameFromLambda(variant.VariantTask):
-    pass
+    def GetUpdate(self, q1=None, q2=None, q3=None, q4=None, **kws):
+        mkm = UnitValue('1 мкм')
+        return dict(
+            mkm=mkm,
+            nu_0=Consts.c_4.Div(mkm, units='Гц', precisionInc=3),
+            nu1=Consts.c.Div(q1, units='Гц', powerShift=3),
+            nu2=Consts.c.Div(q2, units='Гц', powerShift=3),
+            nu3=Consts.c.Div(q3, units='Гц', powerShift=3),
+            nu4=Consts.c.Div(q4, units='Гц', powerShift=3),
+        )
 
 
 @variant.solution_space(80)
@@ -72,11 +95,19 @@ class ColorNameFromLambda(variant.VariantTask):
     Определите {what} колебаний вектора напряженности {of} 
     в электромагнитной волне в вакууме, длина который составляет {lmbd:V:e}. 
 ''')
+@variant.answer_align([
+    '\\lambda &= c T \\implies T = \\frac{ \\lambda }{ c } = \\frac{lmbd:V:s}{Consts.c:V:s} = {T:V},',
+    '\\lambda &= c T = c * \\frac 1{ \\nu } \\implies \\nu = \\frac{ c }{ \\lambda } = \\frac{Consts.c:V:s}{lmbd:V:s} = {nu:V}.',
+])
 @variant.arg(what=['период', 'частоту'])
 @variant.arg(of=['электрического поля', 'индукции магнитного поля'])
 @variant.arg(lmbd=['%d %s' % (l, s) for l in [2, 3, 5] for s in ['м', 'см']])
 class T_Nu_from_lambda(variant.VariantTask):
-    pass
+    def GetUpdate(self, lmbd=None, **kws):
+        return dict(
+            T=lmbd.Div(Consts.c, units='с'),
+            nu=Consts.c.Div(lmbd, units='Гц'),
+        )
 
 
 @variant.solution_space(80)
@@ -84,21 +115,36 @@ class T_Nu_from_lambda(variant.VariantTask):
     Определите энергию фотона излучения частотой {nu:V:e}. 
     Ответ получите в джоулях и в электронвольтах.
 ''')
+@variant.answer_short('E = h \\nu = {Consts.h:V} * {nu:V} \\approx {E:V} \\approx {E_eV:V}')
 @variant.arg(nu=('{} 10^16 Гц', [4, 5, 6, 7, 8, 9]))
 class E_from_nu(variant.VariantTask):
-    pass
+    def GetUpdate(self, nu=None, **kws):
+        E = Consts.h.Mult(nu, units='Дж', precisionInc=1)
+        E_eV = E.Div(Consts.e, units='эВ')
+        return dict(
+            E=E,
+            E_eV=E_eV,
+        )
 
+# 
 
 @variant.solution_space(80)
 @variant.text('''
     Определите энергию {of} с длиной волны {lmbd:V:e}. Ответ выразите в {in_what}.
     Способен ли человеческий глаз увидеть один такой квант? А импульс таких квантов?'
 ''')
+@variant.answer_short('E = h \\nu = \\frac{ hc }{ \\lambda } = \\frac{ {Consts.h:V} * {Consts.c:V} }{lmbd:V:s} \\approx {E:V} \\approx {E_eV:V}')
 @variant.arg(lmbd=('{} нм', [150, 200, 400, 500, 600, 700, 850, 900]))
 @variant.arg(of=['кванта света', 'фотона'])
 @variant.arg(in_what=['джоулях', 'электронвольтах'])
 class E_from_lambda(variant.VariantTask):
-    pass
+    def GetUpdate(self, lmbd=None, **kws):
+        E = Consts.h.Mult(Consts.c, units='Дж').Div(lmbd, units='Дж')
+        E_eV = E.Div(Consts.e, units='эВ')
+        return dict(
+            E=E,
+            E_eV=E_eV,
+        )
 
 
 @variant.solution_space(40)
