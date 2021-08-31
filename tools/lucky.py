d11e93c47f67d77d3802703edb4aa5e874ae7dcd
Mikhail Burmistrov <burmisha@gmail.com>
2021-09-01 00:32:53 +0300

    Add sort options

--- tools/lucky.py
+++ tools/lucky.py
@@ -8,14 +8,17 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def get_lucky(grade=None, count=None, seed=None):
+def get_lucky(grade=None, count=None, seed=None, sort=False):
     now_ts = int(time.time())
     if not seed:
         seed = now_ts
     className = library.datetools.formatTimestamp(now_ts, f'%Y-%m-%d {grade}')
     pupilsList = list(library.pupils.get_class_from_string(className).Iterate())
-    random.seed(seed)
-    random.shuffle(pupilsList)
+    if sort:
+        pupilsList.sort()
+    else:
+        random.seed(seed)
+        random.shuffle(pupilsList)
     if count:
         pupilsList = pupilsList[:count]
     for index, pupil in enumerate(pupilsList, 1):
@@ -24,11 +27,12 @@ def get_lucky(grade=None, count=None, seed=None):
 
 
 def run(args):
-    get_lucky(grade=args.grade, count=args.count, seed=args.seed)
+    get_lucky(grade=args.grade, count=args.count, seed=args.seed, sort=args.sort)
 
 
 def populate_parser(parser):
-    parser.add_argument('-g', '--grade', help='Grade', type=int, choices=[9, 10])
+    parser.add_argument('-g', '--grade', help='Grade', choices=[9, 10, '11БА', '11Б'])
     parser.add_argument('-c', '--count', help='Count', type=int)
     parser.add_argument('-s', '--seed', help='Random seed (None or 0 is now)', type=int, default=0)
+    parser.add_argument('--sort', help='Sort', action='store_true')
     parser.set_defaults(func=run)
