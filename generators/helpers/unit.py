9abd7962a8ab0e5dc22d9c18fbc7ca7227563de9
Mikhail Burmistrov <burmisha@gmail.com>
2021-10-22 22:30:10 +0300

    Start using units

--- generators/helpers/unit.py
+++ generators/helpers/unit.py
@@ -15,8 +15,118 @@ SI_PREFIXES = {
     'д': -1,
 }
 
+class BaseUnit:
+    def __init__(self, name, full_name):
+        assert isinstance(name, str)
+        assert isinstance(full_name, str)
+        self._name = name
+        self._full_name = full_name
+
+
+class BaseUnits:
+    kg = BaseUnit('кг', 'килограм'),
+    s = BaseUnit('с', 'секунда'),
+    m = BaseUnit('м', 'метр'),
+    mol = BaseUnit('моль', 'моль'),
+    A = BaseUnit('А', 'ампер'),
+    K = BaseUnit('К', 'кельвин'),
+    cd = BaseUnit('кд', 'кандела'),
+
+
+class SimpleUnit:
+    def __init__(self, full_name, short_name, en_name, basic_units):
+        assert isinstance(full_name, str)
+        assert isinstance(short_name, str)
+        assert isinstance(en_name, str)
+        assert isinstance(basic_units, dict)
+        self._full_name = full_name
+        self._short_name = short_name
+        self._en_name = en_name
+        self._basic_units = basic_units
+
+
+class SimpleUnits:
+    # base units
+    kg = SimpleUnit('килограм', 'кг', 'kg', {BaseUnits.kg: 1})
+    s = SimpleUnit('секунда', 'с', 's', {BaseUnits.s: 1})
+    m = SimpleUnit('метр', 'м', 'm', {BaseUnits.m: 1})
+    mol = SimpleUnit('моль', 'моль', 'mol', {BaseUnits.mol: 1})
+    A = SimpleUnit('ампер', 'А', 'A', {BaseUnits.A: 1})
+    K = SimpleUnit('кельвин', 'К', 'K', {BaseUnits.K: 1})
+    cd = SimpleUnit('кандела', 'кд', 'cd', {BaseUnits.cd: 1})
+
+    # see https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D0%BD%D1%8B%D0%B5_%D0%B5%D0%B4%D0%B8%D0%BD%D0%B8%D1%86%D1%8B_%D0%A1%D0%98
+    radian = SimpleUnit('радиан', 'рад', 'rad', {})
+    steradian = SimpleUnit('стерадиан', 'ср', 'sr', {})
+    degree_celsius = SimpleUnit('градус Цельсия', '°C', '°C', {BaseUnits.K: 1})
+    hertz = SimpleUnit('герц', 'Гц', 'Hz', {BaseUnits.s: 1})
+    newton = SimpleUnit('ньютон', 'Н', 'N', {BaseUnits.kg: 1, BaseUnits.m: 1, BaseUnits.s: -2})
+    joule = SimpleUnit('джоуль', 'Дж', 'J', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -2})
+    watt = SimpleUnit('ватт', 'Вт', 'W', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -3})
+    pascal = SimpleUnit('паскаль', 'Па', 'Pa', {BaseUnits.kg: 1, BaseUnits.m: -1, BaseUnits.s: -2})
+    lumen = SimpleUnit('люмен', 'лм', 'lm', {BaseUnits.cd: 1})
+    lux = SimpleUnit('люкс', 'лк', 'lx', {BaseUnits.cd: 1, BaseUnits.m: -2})
+    coulomb = SimpleUnit('кулон', 'Кл', 'C', {BaseUnits.A: 1, BaseUnits.s: 1})
+    volt = SimpleUnit('вольт', 'В', 'V', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -3, BaseUnits.A: -1})
+    ohm = SimpleUnit('ом', 'Ом', 'Ω', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -3, BaseUnits.A: -2})
+    farad = SimpleUnit('фарад', 'Ф', 'F', {BaseUnits.s: 4, BaseUnits.A: 2, BaseUnits.kg: -1, BaseUnits.m: -2})
+    weber = SimpleUnit('вебер', 'Вб', 'Wb', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -2, BaseUnits.A: -2})
+    tesla = SimpleUnit('тесла', 'Тл', 'T', {BaseUnits.kg: 1, BaseUnits.s: -2, BaseUnits.A: -1})
+    henry = SimpleUnit('генри', 'Гн', 'H', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -2, BaseUnits.A: -2})
+    siemens = SimpleUnit('сименс', 'См', 'S', {BaseUnits.kg: -1, BaseUnits.m: -2, BaseUnits.s: 3, BaseUnits.A: 2})
+    becquerel = SimpleUnit('беккерель', 'Бк', 'Bq', {BaseUnits.s: -1})
+    gray = SimpleUnit('грей', 'Гр', 'Gy', {BaseUnits.m: 2, BaseUnits.s: -2})
+    sievert = SimpleUnit('зиверт', 'Зв', 'Sv', {BaseUnits.m: 2, BaseUnits.s: -2})
+
+
+
+# TODO: desirable
+def test_value():
+    Value('10 г') + Value('5 см^3') * Value('2000 кг / м^3') == Value('20 г')
+
+
+
+
+def get_known_units():
+    units = [
+        ('час', 3600, SimpleUnits.s),
+        ('сут', 86400, SimpleUnits.s),
+        ('атм', 100000, SimpleUnits.pascal),
+        ('эВ', 1.6 * 10 ** -19, SimpleUnits.joule),  # электрон-вольт
+        ('С', 1, SimpleUnits.degree_celsius),   # цельсий
+        ('C', 1, SimpleUnits.degree_celsius),   # celsium
+        ('К', 1, SimpleUnits.K),   # кельвин
+        ('K', 1, SimpleUnits.K),   # kelvin
+        SimpleUnits.volt, 
+        SimpleUnits.joule,
+        SimpleUnits.newton, 
+        SimpleUnits.watt,
+        SimpleUnits.ohm,
+        SimpleUnits.farad, 
+        SimpleUnits.A,
+        SimpleUnits.coulomb,
+        SimpleUnits.kg,
+        SimpleUnits.s,
+        SimpleUnits.m,
+        SimpleUnits.tesla,
+        SimpleUnits.mol,
+        SimpleUnits.henry,
+        SimpleUnits.hertz,
+        ('г', 1 / 1000, SimpleUnits.kg),   # грам
+        ('т', 1000, SimpleUnits.kg),   # тонна
+    ]
+    for row in units:
+        if isinstance(row, SimpleUnit):
+            yield row._short_name, 1, row
+        else:
+            yield row
+
+
+KNOWN_UNITS = list(get_known_units())
+
 
 class OneUnit:
+
     def __init__(self, line, is_numenator):
         try:
             si_power, human_unit, human_power = self._parse_line(line)
@@ -47,39 +157,17 @@ class OneUnit:
 
         prefix = ''
         main = line
-        for suffix in [
-            'час',
-            'сут',
-            'атм',
-            'эВ',  # электрон-вольт
-            'В',   # вольт
-            'Дж',  # джоуль
-            'Н',   # ньютон
-            'Вт',  # ватт
-            'Ом',  # ом
-            'Ф',   # фарад
-            'А',   # ампер
-            'Кл',  # кулон
-            'кг',  # килограм
-            'г',   # грам
-            'с',   # секунда
-            'м',   # метр
-            'Тл',  # тесла
-            'т',   # тонна
-            'С',   # цельсий
-            'C',   # celsium
-            'К',   # кельвин
-            'K',   # kelvin
-            'моль',
-            'Гн',
-            'Гц',
-        ]:
+        for suffix, multiplier, simple_unit in KNOWN_UNITS:
             if line.endswith(suffix):
                 main = suffix
                 prefix = line[:-len(suffix)]
                 break
 
-        exponent = SI_PREFIXES[prefix]
+        try:
+            exponent = SI_PREFIXES[prefix]
+        except:
+            log.error(f'Could not get exponent for {line} from {prefix}')
+            raise
 
         if main == 'г':
             main = 'кг'
@@ -89,64 +177,20 @@ class OneUnit:
         return exponent * power, line, power
 
 
-class BaseUnit:
-    def __init__(self, name, full_name):
-        self._name = name
-        self._full_name = full_name
+def test_one_unit():
+    data = [
+        ('мВ', -3, 'мВ', 1),
+        ('мВт', -3, 'мВт', 1),
+        ('МэВ', 6, 'МэВ', 1),
+        ('мс^2', -6, 'мс', 2),
+        ('кг^2', 0, 'кг', 2),
+        ('мг^2', -12, 'мг', 2),
+    ]
+    for unit_text, si_power, human_unit, human_power in data:
+        unit = OneUnit(unit_text, True)
+        assert unit.SiPower == si_power, 'Expected {si_power}, got {unit.SiPower}'
+        assert unit.HumanUnit == human_unit, 'Expected {human_unit}, got {unit.HumanUnit}'
+        assert unit.HumanPower == human_power, 'Expected {human_power}, got {unit.HumanPower}'
 
 
-class BaseUnits:
-    kg = BaseUnit('кг', 'килограм'),
-    s = BaseUnit('с', 'секунда'),
-    m = BaseUnit('м', 'метр'),
-    mol = BaseUnit('моль', 'моль'),
-    A = BaseUnit('А', 'ампер'),
-    K = BaseUnit('К', 'кельвин'),
-    cd = BaseUnit('кд', 'кандела'),
-
-
-class SimpleUnit:
-    def __init__(self, full_name, short_name, en_name, basic_units):
-        self._full_name = full_name
-        self._short_name = short_name
-        self._en_name = en_name
-        self._basic_units = basic_units
-
-
-class SimpleUnits:
-    # base units
-    kg = SimpleUnit('килограм', 'кг', 'kg', {BaseUnits.kg: 1})
-    s = SimpleUnit('секунда', 'с', 's', {BaseUnits.s: 1})
-    m = SimpleUnit('метр', 'м', 'm', {BaseUnits.m: 1})
-    mol = SimpleUnit('моль', 'моль', 'mol', {BaseUnits.mol: 1})
-    A = SimpleUnit('ампер', 'А', 'A', {BaseUnits.A: 1})
-    K = SimpleUnit('кельвин', 'К', 'K', {BaseUnits.K: 1})
-    cd = SimpleUnit('кандела', 'кд', 'cd', {BaseUnits.cd: 1})
-
-    # see https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D0%BD%D1%8B%D0%B5_%D0%B5%D0%B4%D0%B8%D0%BD%D0%B8%D1%86%D1%8B_%D0%A1%D0%98
-    radian = SimpleUnit('радиан', 'рад', 'rad', [])
-    steradian = SimpleUnit('стерадиан', 'ср', 'sr', [])
-    degree_celsius = SimpleUnit('градус Цельсия', '°C', '°C', {BaseUnits.K: 1})
-    hertz = SimpleUnit('герц', 'Гц', 'Hz', {BaseUnits.s: 1})
-    newton = SimpleUnit('ньютон', 'Н', 'N', {BaseUnits.kg: 1, BaseUnits.m: 1, BaseUnits.s: -2})
-    joule = SimpleUnit('джоуль', 'Дж', 'J', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -2})
-    watt = SimpleUnit('ватт', 'Вт', 'W', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -3})
-    pascal = SimpleUnit('паскаль', 'Па', 'Pa', {BaseUnits.kg: 1, BaseUnits.m: -1, BaseUnits.s: -2})
-    lumen = SimpleUnit('люмен', 'лм', 'lm', {BaseUnits.cd: 1})
-    lux = SimpleUnit('люкс', 'лк', 'lx', {BaseUnits.cd: 1, BaseUnits.m: -2})
-    coulomb = SimpleUnit('кулон', 'Кл', 'C', {BaseUnits.A: 1, BaseUnits.s: 1})
-    volt = SimpleUnit('вольт', 'В', 'V', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -3, BaseUnits.A: -1})
-    ohm = SimpleUnit('ом', 'Ом', 'Ω', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -3, BaseUnits.A: -2})
-    farad = SimpleUnit('фарад', 'Ф', 'F', {BaseUnits.s: 4, BaseUnits.A: 2, BaseUnits.kg: -1, BaseUnits.m: -2})
-    weber = SimpleUnit('вебер', 'Вб', 'Wb', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -2, BaseUnits.A: -2})
-    tesla = SimpleUnit('тесла', 'Тл', 'T', {BaseUnits.kg: 1, BaseUnits.s: -2, BaseUnits.A: -1})
-    henry = SimpleUnit('генри', 'Гн', 'H', {BaseUnits.kg: 1, BaseUnits.m: 2, BaseUnits.s: -2, BaseUnits.A: -2})
-    siemens = SimpleUnit('сименс', 'См', 'S', {BaseUnits.kg: -1, BaseUnits.m: -2, BaseUnits.s: 3, BaseUnits.A: 2})
-    becquerel = SimpleUnit('беккерель', 'Бк', 'Bq', {BaseUnits.s: -1})
-    gray = SimpleUnit('грей', 'Гр', 'Gy', {BaseUnits.m: 2, BaseUnits.s: -2})
-    sievert = SimpleUnit('зиверт', 'Зв', 'Sv', {BaseUnits.m: 2, BaseUnits.s: -2})
-
-
-# TODO: desirable
-def test_value():
-    Value('10 г') + Value('5 см^3') * Value('2000 кг / м^3') == Value('20 г')
+test_one_unit()
