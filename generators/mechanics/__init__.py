1f86d924b4c20b4b9d861e575ad5a6009889504d
Mikhail Burmistrov <burmisha@gmail.com>
2021-11-01 02:06:44 +0300

    Move waves

--- generators/mechanics/__init__.py
+++ generators/mechanics/__init__.py
@@ -1,6 +1,5 @@
 import generators.mechanics.dynamics
 import generators.mechanics.gidro
 import generators.mechanics.kinematics
-import generators.mechanics.waves
 import generators.mechanics.zsi_zse
 import generators.mechanics.statics
--- generators/oscillation/__init__.py
+++ generators/oscillation/__init__.py
@@ -1,3 +1,5 @@
 import generators.oscillation.basic
 import generators.oscillation.electromagnet
 import generators.oscillation.mechanical
+import generators.oscillation.waves
+
