49fabf074dee6a621e8bd3086f799c1b5bd2c2a2
Mikhail Burmistrov <burmisha@gmail.com>
2021-11-09 21:33:41 +0300

    Delete duplicated template

--- generators/oscillation/mechanical.py
+++ generators/oscillation/mechanical.py
@@ -2,15 +2,6 @@ import generators.variant as variant
 from generators.helpers import letter_variants, Fraction, Consts, Decimal
 import math
 
-@variant.text('''
-''')
-@variant.solution_space(80)
-@variant.arg(A=('A = {} a', [1]))
-@variant.answer_align([
-])
-class Template(variant.VariantTask):
-    pass
-
 
 @variant.text('''
     Тело колеблется по гармоническому закону,
