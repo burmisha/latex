671eb9884fd2eea6f72911e4d88d063e4d1a1f72
Mikhail Burmistrov <burmisha@gmail.com>
2021-11-16 18:41:47 +0300

    Add new tasks

--- generators/oscillation/__init__.py
+++ generators/oscillation/__init__.py
@@ -1,4 +1,5 @@
 import generators.oscillation.basic
+import generators.oscillation.dc_transform
 import generators.oscillation.electromagnet
 import generators.oscillation.em_waves
 import generators.oscillation.mechanical
