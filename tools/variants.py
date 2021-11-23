ad6cd4e30e2afede6d69fb536bad03491cddaca9
Mikhail Burmistrov <burmisha@gmail.com>
2021-11-23 14:20:36 +0300

    Add more variants

--- tools/variants.py
+++ tools/variants.py
@@ -651,7 +651,7 @@ def get_all_variants():
         Work(
             task_id='2021-11-18 11БА - ВО - 1',
             classes=[
-                {'optics.wave': ['Vishnyakova_3_6_12', 'Vishnyakova_3_6_14', 'Vishnyakova_3_6_15', 'Vishnyakova_3_6_16', 'Vishnyakova_3_6_17', 'Vishnyakova_3_6_18', ]},
+                {'optics.wave': ['Vishnyakova_example_11', 'Vishnyakova_3_6_12', 'Vishnyakova_3_6_14', 'Vishnyakova_3_6_15', 'Vishnyakova_3_6_16', 'Vishnyakova_3_6_17', 'Vishnyakova_3_6_18', ]},
                 {'optics.geom': [
                     'Vishnyakova_3_6_1', 'Vishnyakova_3_6_2', 'Vishnyakova_3_6_4', 'Vishnyakova_3_6_5', 'Vishnyakova_3_6_6',
                     'Vishnyakova_3_6_7', 'Vishnyakova_3_6_8', 'Vishnyakova_3_6_9', 'Vishnyakova_3_6_10', 'Vishnyakova_3_6_11'
