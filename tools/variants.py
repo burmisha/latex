d42c15fa5e16cc1fe568c9abb0b80738cc3d8996
Mikhail Burmistrov <burmisha@gmail.com>
2021-12-07 21:55:02 +0300

    Add variants

--- tools/variants.py
+++ tools/variants.py
@@ -658,13 +658,29 @@ def get_all_variants():
         Work(
             task_id='2021-11-24 11БА - ВО - 1',
             classes=[
-                {'optics.interference': ['Gendenshteyn_11_22_4', 'Vishnyakova_example_11', 'Vishnyakova_3_6_12', 'Vishnyakova_3_6_14', 'Belolipetsky_5_196']}, 
+                {'optics.interference': ['Gendenshteyn_11_22_4', 'Vishnyakova_example_11', 'Vishnyakova_3_6_12', 'Vishnyakova_3_6_14', 'Belolipetsky_5_196']},
             ],
         ),
         Work(
             task_id='2021-12-02 11БА - ВО - 2',
             classes=[
-                {'optics.difraction': ['Vishnyakova_3_6_15', 'Vishnyakova_3_6_16', 'Vishnyakova_3_6_17', 'Vishnyakova_3_6_18']}, 
+                {'optics.difraction': ['Vishnyakova_3_6_15', 'Vishnyakova_3_6_16', 'Vishnyakova_3_6_17', 'Vishnyakova_3_6_18']},
+            ],
+        ),
+        Work(
+            task_id='2021-12-08 11БА - ВО - 3',
+            classes=[
+                {'optics.interference': ['Task01', 'Task02', 'Vishnyakova_example_11']},
+                {'optics.difraction': ['Vishnyakova_3_6_18']},
+                {'optics.geom': ['Vishnyakova_3_6_1', 'Vishnyakova_3_6_2']},
+            ],
+        ),
+        Work(
+            task_id='2021-12-08 11Б - ВО - 1',
+            classes=[
+                {'oscillation.em_waves': ['Definitions01', 'Deduce01']},
+                {'optics.geom': ['Vishnyakova_3_6_2']},
+                {'optics.interference': ['Gendenshteyn_11_22_4', 'Vishnyakova_3_6_12', 'Vishnyakova_example_11']},
             ],
         ),
     ]
