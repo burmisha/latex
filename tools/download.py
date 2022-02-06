b9430f00d872aee3bce8b96d8abbdc0cd25fda4f
Mikhail Burmistrov <burmisha@gmail.com>
2022-02-06 21:17:38 +0300

    Add old video

--- tools/download.py
+++ tools/download.py
@@ -159,6 +159,7 @@ def run(args):
             'https://www.youtube.com/watch?v=i8fD_jG_TM8': 'FREE BEATS',
             'https://www.youtube.com/watch?v=LXpfBwApM3E': '2021 - 554 - 1 класс',
             'https://www.youtube.com/watch?v=pySJ3cNrZKA': '2022 - Вынос ели',
+            'https://www.youtube.com/watch?v=rckEw0z7aK4': '1964 - Что такое теория относительности'
         },
         'Veritasium': {
             'https://www.youtube.com/watch?v=AaZ_RSt0KP8': '2021.08.31 - The Universe is Hostile to Computers',
