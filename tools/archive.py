699ad6177dc86c6f135acf3f16b62d7eaf9e98d5
Mikhail Burmistrov <burmisha@gmail.com>
2022-03-24 14:19:16 +0300

    Delete pupils from git

--- tools/archive.py
+++ tools/archive.py
@@ -9,37 +9,50 @@ import base64
 import time
 
 
-def run(args):
-    src_file = library.location.root('data', 'marks.yaml')
+def archive(src_file):
+    archive_file = f'{src_file}.archived'
+    
+    with open(archive_file) as f:
+        data = f.read()
+
+    t = base64.decodebytes(data.encode())
+    decoded = zlib.decompress(t).decode()
+
+    now = int(time.time())
+    t_file = f'{src_file}.{now}'
+    log.info(f'Extract: {t_file}')
+    with open(t_file, 'w') as f:
+        f.write(decoded)
+
+    cmd = f'vimdiff {src_file} {t_file}'
+    library.process.pbcopy(cmd)
+
+
+def unarchive(src_file):
     archive_file = f'{src_file}.archived'
 
-    if args.extract:
-        with open(archive_file) as f:
-            data = f.read()
+    with open(src_file) as f:
+        data = f.read()
 
-        t = base64.decodebytes(data.encode())
-        decoded = zlib.decompress(t).decode()
+    t = zlib.compress(data.encode())
+    encoded = base64.encodebytes(t)
 
-        now = int(time.time())
-        t_file = f'{src_file}.{now}'
-        log.info(f'Extract: {t_file}')
-        with open(t_file, 'w') as f:
-            f.write(decoded)
+    log.info(f'Archive:\n\tsrc:\t{src_file}\n\tdst:\t{archive_file}')
+    with open(archive_file, 'wb') as f:
+        f.write(encoded)
 
-        cmd = f'vimdiff {src_file} {t_file}'
-        library.process.pbcopy(cmd)
 
-    else:
-        with open(src_file) as f:
-            data = f.read()
 
-        t = zlib.compress(data.encode())
-        encoded = base64.encodebytes(t)
-
-        log.info(f'Archive:\n\tsrc:\t{src_file}\n\tdst:\t{archive_file}')
-        with open(archive_file, 'wb') as f:
-            f.write(encoded)
-
+def run(args):
+    files = [
+        library.location.root('data', 'marks.yaml'),
+        library.location.root('data', 'pupils.yaml'),
+    ]
+    for file in files:
+        if args.extract:
+            archive(file)
+        else:
+            unarchive(file)
 
 
 
