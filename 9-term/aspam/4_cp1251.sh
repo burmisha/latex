d3d1766e65e62940549ec448d70b8f583bec2bf5
Mikhail Burmistrov <burmisha@gmail.com>
2022-04-10 13:56:26 +0300

    Less tabs

--- 9-term/aspam/4_cp1251.sh
+++ 9-term/aspam/4_cp1251.sh
@@ -5,14 +5,14 @@ file="BurmistrovSanduleanu2012SpamText_cp1251.tex"
 >$file
 for line in $(cat <BurmistrovSanduleanu2012SpamText.tex | sed 's|utf8|cp1251|g' | sed 's|%.*$||g')
 do
-	# echo $line
+    # echo $line
     if [ "${line:1:5}" = "input" ]; then
-    	# f =
-    	echo " " >>$file
-    	cat $(echo $line | sed 's|\\input{||' | sed 's|}||' |sed 's|.tex||'| awk '{print $1".tex"}') >>$file
-    	echo " " >>$file
+        # f =
+        echo " " >>$file
+        cat $(echo $line | sed 's|\\input{||' | sed 's|}||' |sed 's|.tex||'| awk '{print $1".tex"}') >>$file
+        echo " " >>$file
     else
-    	printf "%s " $line >>$file
-    	echo " " >>$file
+        printf "%s " $line >>$file
+        echo " " >>$file
     fi
 done
